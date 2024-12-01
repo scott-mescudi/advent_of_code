daa90517d88da07178bf79275eadc948e26fbb7a
scott-mescudi <anish.chittu2007@gmail.com>
2024-12-01 18:17:05 +0100

    Remove ignored files from repository

--- 2024/day1/star1/target/debug/deps/star1-c80994d8f3330d83.d
+++
@@ -1,5 +0,0 @@
-/run/media/astr0/astr0/advent_of_code/2024/day1/star1/target/debug/deps/star1-c80994d8f3330d83: main.rs
-
-/run/media/astr0/astr0/advent_of_code/2024/day1/star1/target/debug/deps/star1-c80994d8f3330d83.d: main.rs
-
-main.rs:
