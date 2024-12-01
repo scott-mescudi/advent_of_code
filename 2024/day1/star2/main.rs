e63e1b2a0b97edcab974e61ab63966022af46f69
scott-mescudi <anish.chittu2007@gmail.com>
2024-12-01 18:22:36 +0100

    mbv

---
+++ 2024/day1/star2/main.rs
@@ -0,0 +1,66 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+struct Pairs {
+    left: Vec<i32>,
+    right: Vec<i32>,
+}
+
+fn parse_data(filename: String) -> Pairs {
+    let path = Path::new(&filename);
+    let file = match File::open(path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Error opening file: {}", e);
+            return Pairs { left: vec![], right: vec![] };
+        }
+    };
+
+    let reader = io::BufReader::new(file);
+
+    let mut lnums: Vec<i32> = Vec::new();
+    let mut rnums: Vec<i32> = Vec::new();
+
+    for line in reader.lines(){
+        match line {
+            Ok(content) => {
+                let parts: Vec<&str> = content.split_whitespace().collect();
+                let value1: i32 = parts[0].parse().unwrap();
+                let value2: i32 = parts[1].parse().unwrap();
+
+                lnums.push(value1);
+                rnums.push(value2);
+            }
+            Err(e) => {
+                eprintln!("{}", e);
+            }
+        }
+    }
+
+    lnums.sort();
+    rnums.sort();
+
+    return Pairs { left: lnums, right: rnums };
+}
+
+fn main() {
+    let data = parse_data(String::from("../testdata.txt"));
+
+    let len = data.left.len();
+    let mut total: i32 = 0;
+
+    for i in 0..len {
+        let mut t1: i32 = 0;
+        for j in 0..len {
+            if data.left[i] == data.right[j] {
+                t1 += 1;
+            }
+        }
+
+        total += data.left[i] * t1
+    }
+
+    println!("total is {}", total);
+
+}
---
+++ 2024/day1/star2/target/debug/star2.d
@@ -0,0 +1,1 @@
+/run/media/astr0/astr0/advent_of_code/2024/day1/star2/target/debug/star2: /run/media/astr0/astr0/advent_of_code/2024/day1/star2/main.rs
