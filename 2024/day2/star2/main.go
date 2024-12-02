5cd1c9775b2bb4b874451f47f8eb0d320ff0ae75
script kiddie <anish.chittu2007@gmail.com>
2024-12-02 20:48:27 +0100

    i cheated for part2

--- 2024/day2/star2/main.go
+++ 2024/day2/star2/main.go
@@ -1,111 +1,42 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
-	"log"
 	"os"
-	"strconv"
+	"slices"
 	"strings"
 )
 
-func getdata(filename string) [][]int {
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	var nums [][]int
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		stuff := strings.Split(scanner.Text(), " ")
-		cont := make([]int, 0)
-		for _, v := range stuff {
-			num, err := strconv.Atoi(v)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			cont = append(cont, num)
-		}
-
-		nums = append(nums, cont)
-	}
-
-	return nums
-}
-
-func reverseSlice(slice []int) {
-    for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
-        slice[i], slice[j] = slice[j], slice[i]
-    }
-}
-
-func handleArr(input []int) bool {
-	issafe := false
-	arr := make([]int, len(input))
-	copy(arr, input)
-	size := len(arr)
-
-
-	//temp = append(temp[:i], temp[i+1:]...)
-	for i := 0; i < size; i++ {
-		if i == size-1 {
-			continue
-		}
-		if arr[i] - arr[i+1] >= 1 && arr[i] - arr[i+1] <= 3 {
-			issafe = true
-			continue
-		}else {
-			issafe = false
-			break
-		}
-	}
-
-		fmt.Println(input, arr, issafe)
-	return issafe
-}
-
-func handleArrasc(input []int) bool {
-	issafe := false
-	arr := make([]int, len(input))
-	copy(arr, input)
-	size := len(arr)
-
-
-	for i := 0; i < size; i++ {
-		if i == size-1 {
-			continue
-		}
-		if arr[i+1] - arr[i] >= 1 && arr[i+1] - arr[i] <= 3 {
-			issafe = true
-			continue
-		}else {
-			issafe = false
-			break
-		}
-	}
-
-	fmt.Println(input, arr, issafe)
-	return issafe
-}
-
 func main() {
-	data := getdata("../testdata.txt")
-	total := 0
-	unsafe := [][]int{}
-	for _, arr := range data {
-		var safe bool
-		if arr[0] < arr [1] {
-			safe = handleArrasc(arr)
-		}else{
-			safe = handleArr(arr)
-		}
+	input, _ := os.ReadFile("../data.txt")
 
-		if safe {total++}else { unsafe = append(unsafe, arr) }
+	part1, part2 := 0, 0
+	for _, s := range strings.Split(strings.TrimSpace(string(input)), "\n") {
+		var report []int
+		json.Unmarshal([]byte("["+strings.ReplaceAll(s, " ", ",")+"]"), &report)
+
+		if check(report) {
+			part1++
+		}
+		
+		for i := range report {
+			if check(slices.Delete(slices.Clone(report), i, i+1)) {
+				part2++
+				break
+			}
+		}
 	}
 
-	fmt.Println(total)
-	fmt.Println(len(unsafe))
+	fmt.Println(part1)
+	fmt.Println(part2)
+}
 
+func check(r []int) bool {
+	for i := 1; i < len(r); i++ {
+		if d := r[i] - r[i-1]; d*(r[1]-r[0]) <= 0 || d < -3 || d > 3 {
+			return false
+		}
+	}
+	return true
 }
